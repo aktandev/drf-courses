9580e17a3486e36d515964aac50150f3c2cc047a
ak7an <aktandev@gmail.com>
2022-06-28 18:48:49 +0600

    Automatic configuration with django-heroku

--- CoursesApp/courses/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Course)
-admin.site.register(Category)
-admin.site.register(Contact)
-admin.site.register(Branch)
-
--- CoursesApp/courses/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from courses import views
-
-urlpatterns = [
-    path('api/v1/courses/', views.CoursesAPIList.as_view(), name='courses'),
-    path('api/v1/courses/<int:pk>/', views.CoursesAPIView.as_view(), name='course'),
-]
