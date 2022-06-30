62d198e07494d4ce85f26e93159d2660649385c6
ak7an <aktandev@gmail.com>
2022-07-01 02:53:11 +0600

    correction in settings

--- CoursesApp/settings.py
+++ CoursesApp/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'PORT': config('DB_PORT'),
     }
 }
-prod_db = dj_database_url.config(conn_max_age=500)
+prod_db = dj_database_url.config()
 DATABASES['default'].update(prod_db)
 
 
@@ -96,40 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'mysite.log',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'MYAPP': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-        },
-    }
-}
 
 
 LANGUAGE_CODE = 'en-us'
